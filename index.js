3bbaa0b37d6394351832435b54eb6939d2a2fd69
nedoratic <nedoratic@gmail.com>
2024-03-20 20:18:18 +0100

    get median function added

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Get Mean
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+// Get Median
+const getMedian = (array) => {
+	const sorted = array.sort((a, b) => a - b);
+	const median = array.length % 2 === 0 ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]]) : sorted[Math.floor(array.length / 2)];
+	return median;
+};
