08dd6ee8e731e0b6a1bf43c943dcd7ab808fb286
nedoratic <nedoratic@gmail.com>
2024-03-20 20:20:02 +0100

    get mode function added

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ const getMedian = (array) => {
 	const median = array.length % 2 === 0 ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]]) : sorted[Math.floor(array.length / 2)];
 	return median;
 };
+
+// Get Mode
+const getMode = (array) => {
+	const counts = {};
+	array.forEach((el) => {
+		counts[el] = (counts[el] || 0) + 1;
+	});
+	if (new Set(Object.values(counts)).size === 1) {
+		return null;
+	}
+	const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+	const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
+	return mode.join(', ');
+};
